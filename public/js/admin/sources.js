fdbd30f19b8e5726b934d8035227bf0535ed0b6f
twinh <twinhuang@qq.com>
2017-09-19 10:32:44 +0800

    更新压缩前端素材

--- public/js/admin/sources.js
+++ public/js/admin/sources.js
@@ -1,8 +1,11 @@
 define([
   'template',
+  'linkTo',
   'form',
   'validator',
-  'dataTable'
+  'dataTable',
+  'daterangepicker',
+  'plugins/app/libs/jquery.populate/jquery.populate',
 ], function (template) {
   var Sources = function () {
     this.data = {};
@@ -14,16 +17,72 @@ define([
   };
 
   Sources.prototype.indexAction = function () {
-    var $recordTable = this.$('.js-source-table').dataTable({
+    var $table = this.$('.js-source-table').dataTable({
       ajax: {
         url: $.queryUrl('admin/sources.json')
       },
       columns: [
         {
+          data: 'id',
+          visible: false
+        },
+        {
+          data: 'code'
+        },
+        {
           data: 'name'
         },
         {
-          data: 'updated_at'
+          data: 'view_count',
+          sortable: true
+        },
+        {
+          data: 'subscribe_user',
+          sortable: true
+        },
+        {
+          data: 'unsubscribe_user',
+          sortable: true
+        },
+        {
+          data: 'net_subscribe_value',
+          sortable: true
+        },
+        {
+          data: 'consume_member_user',
+          sortable: true
+        },
+        {
+          data: 'receive_member_count',
+          sortable: true
+        },
+        {
+          data: 'receive_card_count',
+          sortable: true
+        },
+        {
+          data: 'consume_card_count',
+          sortable: true
+        },
+        {
+          data: 'add_score_value',
+          sortable: true
+        },
+        {
+          data: 'sub_score_value',
+          sortable: true
+        },
+        {
+          data: 'id',
+          render: function (data, type, full) {
+            return template.render('js-qrcode-scene-id-tpl', full);
+          }
+        },
+        {
+          data: 'created_at',
+          render: function (data) {
+            return data.substr(10) + '<br>' + data.substr(0, 10);
+          }
         },
         {
           data: 'id',
@@ -35,14 +94,32 @@ define([
       ]
     });
 
-    $recordTable.deletable();
+    $table.deletable();
+
+
+    this.$('.js-source-form')
+      .loadParams()
+      .submit(function (e) {
+        $table.reload($(this).serialize(), false);
+        e.preventDefault();
+      });
+
+    // 日期范围选择
+    $('.js-range-date').daterangepicker({
+      format: 'YYYY-MM-DD',
+      separator: ' ~ '
+    }, function (start, end) {
+      $('.js-start-date').val(start.format(this.format));
+      $('.js-end-date').val(end.format(this.format));
+      this.element.trigger('change');
+    });
   };
 
   Sources.prototype.editAction = function (options) {
     $.extend(this, options);
 
     this.$('.js-source-form')
-      .loadJSON(this.data)
+      .populate(this.data)
       .ajaxForm({
         url: $.url('admin/sources/update'),
         dataType: 'json',
@@ -73,6 +150,19 @@ define([
 
     $url.change(updateUrl);
 
+    // 选择预设链接
+    this.$('.js-link-to').linkTo({
+      linkText: '请选择',
+      hide: {
+        keyword: true,
+        decorator: true
+      },
+      update: function (data) {
+        $url.val(window.location.origin + $.url(data.value));
+        updateUrl();
+      }
+    });
+
     // 点击复制活动链接
     this.$('.js-copy').click(function () {
       if (!$result.val()) {
@@ -86,6 +176,12 @@ define([
     });
 
     function updateUrl() {
+      var val = $url.val();
+      if (val === '') {
+        $result.val('');
+        return;
+      }
+
       var param = {};
       param[that.paramName] = that.data.id;
       $result.val($.appendUrl($url.val(), param));
